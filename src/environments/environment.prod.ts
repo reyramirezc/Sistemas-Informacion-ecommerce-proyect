1c6cc1dec1c5bd9a532bcbdb1b98d4783e78c3d0
Nicolas Silveira <nsvh22@gmail.com>
2019-03-21 03:52:07 -0400

    deployment adjustments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  
+  firebaseConfig: {
+    apiKey: "AIzaSyAKA9OUpa6Z68NhY4xQvvrZOUFchM5cOCM",
+    authDomain: "la-mandarina-store.firebaseapp.com",
+    databaseURL: "https://la-mandarina-store.firebaseio.com",
+    projectId: "la-mandarina-store",
+    storageBucket: "la-mandarina-store.appspot.com",
+    messagingSenderId: "164308755010"
+  }
 };
