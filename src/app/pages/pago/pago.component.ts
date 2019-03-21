fec67690e1ee7d19d121635c9c977a4ed42fbef9
Nicolas Silveira <46078503+NikoSilveira@users.noreply.github.com>
2019-03-21 01:43:31 -0400

    Paypal module added

--- src/app/pages/pago/pago.component.ts
+++ src/app/pages/pago/pago.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+
+declare let paypal: any;
 
 @Component({
   selector: 'app-pago',
   templateUrl: './pago.component.html',
   styleUrls: ['./pago.component.css']
 })
-export class PagoComponent implements OnInit {
+export class PagoComponent implements AfterViewChecked {
 
-  constructor() { }
+  addScript: boolean = false;
+  paypalLoad: boolean = true;
 
-  ngOnInit() {
+  finalAmount: number = 2;
+
+  paypalConfig = {
+    env: 'sandbox',
+    client : {
+      sandbox: 'AS-KrV7RLvtAEOw67LnFNfw4lQ7QyeJEbFhiEFuQ--ytcY1dt5oF4J13Np9lbsuZwrS83P2TwnoJPi-5',
+      production: '<your-production-key here>'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment:{
+          transactions: [
+            {amount: {total: this.finalAmount, currency: 'USD'}}
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        //Do something when payment is successful.
+      })
+    }
+  };
+
+  ngAfterViewChecked(): void {
+    if(!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+
+  addPaypalScript(){
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
   }
 
 }
